a63de03b64bf526f3e97eb41527aecb13cb53c00
Skyler_Chou <skyler_chou@donkeymove.com>
2024-01-29 15:05:06 +0800

    章節 1-3 - 架構解析 - pages - 新增頁面

---
+++ app.vue
@@ -0,0 +1,12 @@
+<!-- 所有網頁的進入點 -->
+<template>
+  <div>
+
+    <!-- 預設的歡迎畫面 -->
+    <!-- <NuxtWelcome /> -->
+
+    <!-- 可以想像成 router-view 的功能，才能顯示頁面畫面 -->
+    <NuxtPage />
+
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>首頁</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
