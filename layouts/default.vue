22d49f3dca90de002e4b66a3c7c997f9ed265a8a
Skyler_Chou <skyler_chou@donkeymove.com>
2024-01-29 16:23:23 +0800

    章節 1-4 - 架構解析 - layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <header>123</header>
+    <!-- slot 可以想像成 router-view 的功能，才能顯示頁面畫面 -->
+    <slot />
+    <footer>456</footer>
+  </div>
+</template>
+
+<style scoped></style>
