46a93d2694247752a5805dfae0ecc3033ac6eb62
Skyler_Chou <skyler_chou@donkeymove.com>
2024-01-31 11:17:12 +0800

    章節 1-18 - 架構解析 - ClientOnly

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
   <h1>首頁 : {{ store.count }}</h1>
   <button @click="store.addCount">Add</button>
-  <h2>{{$hello("Skyler")}}</h2>
+  <!-- 
+    如果只要在客戶端渲染，要加上 ClientOnly，不然伺服器會報錯
+    ClientOnly 是可以讓你指定你的DOM只在 Clinet 渲染，不在 Server 端渲染
+  -->
+  <ClientOnly>
+    <h2>{{$hello("Skyler")}}</h2>
+  </ClientOnly>
   <h2 v-timeformat="1680574081915"></h2>
   <VDatePicker v-model='selectedDate' />
   <div></div>
@@ -19,7 +25,7 @@ const { $hello } = useNuxtApp();
 
 const store = useHomeStore();
 
-const selectedDate=ref(new Date());
+const selectedDate = ref(new Date());
 </script>
 
 <style>
