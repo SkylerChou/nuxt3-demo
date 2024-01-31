c717336b3d44709539f5ef0c7680e1e23b41378d
Skyler_Chou <skyler_chou@donkeymove.com>
2024-01-31 16:07:19 +0800

    章節 2-3 - SEO 基礎 - Page Head Setting - useSeoMeta

--- pages/about.vue
+++ pages/about.vue
@@ -6,15 +6,23 @@
 
 <script setup>
 // 參考官方文件 - https://nuxt.com/docs/api/composables/use-head#usehead
-useHead({
-  title: "關於我們 About - Nuxt3 高效入門全攻略",
-  meta: [
-    { property: "og:title", content: "關於我們 About - Nuxt3 高效入門全攻略" },
-    { property: "og:url", content: "http://localhost:3000/about" },
-    { property: "og:image", content: "http://localhost:3000/share.jpg" },
-    { name: "description", content: "關於我們 About - 最棒的Nuxt3的線上課程" },
-    { property: "og:description", content: "關於我們 About - 最棒的Nuxt3的線上課程" },
-  ],
+// useHead({
+//   title: "關於我們 About - Nuxt3 高效入門全攻略",
+//   meta: [
+//     { property: "og:title", content: "關於我們 About - Nuxt3 高效入門全攻略" },
+//     { property: "og:url", content: "http://localhost:3000/about" },
+//     { property: "og:image", content: "http://localhost:3000/share.jpg" },
+//     { name: "description", content: "關於我們 About - 最棒的Nuxt3的線上課程" },
+//     { property: "og:description", content: "關於我們 About - 最棒的Nuxt3的線上課程" },
+//   ],
+// });
+
+// Nuxt 的新方法，可以直接用駝峰命名設定
+useSeoMeta({
+  title: "About - Nuxt3 高效入門全攻略",
+  description: "關於我們 - 最棒的Nuxt3的線上課程",
+  ogDescription: "關於我們 - 最棒的Nuxt3的線上課程",
+  ogTitle: "About - Nuxt3 高效入門全攻略",
 });
 
 </script>
