cb0918182544e13090e528e5b54045997071838f
Skyler_Chou <skyler_chou@donkeymove.com>
2024-01-31 15:31:12 +0800

    章節 2-1 - SEO 基礎 - Global Head Setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // 全域的 meta tag 設定，可參照 https://nuxt.com/docs/api/nuxt-config#head
+  "app": {
+    "head": {
+      "viewport": "width=500, initial-scale=1",
+      "title": "Nuxt3 高效入門全攻略",
+      "meta": [
+        { "name": "description", "content": "這是 Mike 的 Nuxt3 高效入門全攻略課程" },
+        { "property": "og:title", "content": "Nuxt3 高效入門全攻略" },
+        { "property": "og:url", "content": "http://localhost:3000/" },
+        { "property": "og:description", "content": "這是 Mike 的 Nuxt3 高效入門全攻略課程" },
+      ]
+    }
+  },
+
   // 設定只有在 global 下才自動引入
   "components": {
   "dirs": [
