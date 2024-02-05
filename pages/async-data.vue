124a94c81981186fc9d846fee661a9686f0e3763
Skyler_Chou <skyler_chou@donkeymove.com>
2024-02-05 14:00:19 +0800

    章節 3-4 - Server Side Rendering - refreshNuxtData 重新獲取資料的兩個方法

--- pages/async-data.vue
+++ pages/async-data.vue
@@ -1,5 +1,9 @@
 <template>
   <h1>useAsyncData</h1>
+  <!-- 方法一 -->
+  <!-- <button @click="refresh">refresh</button> -->
+  <!-- 方法二 -->
+  <button @click="refreshGetData">refresh</button>
   {{ data }}
 </template>
 
@@ -15,9 +19,37 @@
  * key ：唯一的值，防止在 server 端和 client 端觸發兩次資料獲取，可帶可不帶，
  * 不帶的話會為自動幫你生成一個對應檔案名和編號的唯一 key，但是會建議都要帶。
  */
+
+/**
+ * ----- 方式一 -----
+ * 提供四種狀態
+ * data：回傳的資料。
+ * pending：一個 Boolean，跟你說非同步是否完成了。
+ * refresh：可用於刷新函數返回的資料的 function。
+ * error：如果非同步失敗，回傳錯誤相關的資料
+ */
+// const { data, refresh } = await useAsyncData('nuxt3test', () => {
+//   return $fetch('https://vue-lessons-api.vercel.app/seo/user')
+// })
+
+/** 
+ * ----- 方式二 -----
+ * 使用 refreshNuxtData
+ * 我們就可以透過這個唯一的key使用 refreshNuxtData 
+ * 去重新觸發抓取資料，更新頁面的顯示，而且在其他組件也都可以調用。
+ */
 const { data } = await useAsyncData('nuxt3test', () => {
   return $fetch('https://vue-lessons-api.vercel.app/seo/user')
 })
+
+const refreshGetData = () =>{
+  refreshNuxtData('nuxt3test');
+  console.log('refreshGetData');
+}
+
+
+// 這是會重複在 server 跟 client 觸發，所以不推薦這麼做 
+// const data = await $fetch("https://vue-lessons-api.vercel.app/seo/user");
 
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,13 @@
     ClientOnly 是可以讓你指定你的DOM只在 Clinet 渲染，不在 Server 端渲染
   -->
   <ClientOnly>
-    <h2>{{$hello("Skyler")}}</h2>
+    <h2>{{ $hello("Skyler") }}</h2>
   </ClientOnly>
 
   <NuxtLink to="/user">User</NuxtLink> | <NuxtLink to="/about">About</NuxtLink> | <NuxtLink to="/async-data">Async-data</NuxtLink>
 
   <h2 v-timeformat="1680574081915"></h2>
   <VDatePicker v-model='selectedDate' />
-  <div></div>
 
   <!-- Nuxt 自動 import -->
   <Home />
