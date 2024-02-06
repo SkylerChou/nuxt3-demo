6157afff4f6ec2b1462d4898dba1533201292889
Skyler_Chou <skyler_chou@donkeymove.com>
2024-02-06 12:16:26 +0800

    章節 3-5 - Server Side Rendering - useFetch，多個 API

---
+++ pages/a-lot-of-fetch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>多個 API 請求</h1>
+    <div>{{ orgsData }}</div>
+    <div>------------------</div>
+    <div>{{ reposData }}</div>
+  </div>
+</template>
+
+<script setup>
+
+/**
+ * API 少的時候使用此方式，還不會有什麼問題
+ */
+// const { data: orgsData } = await useFetch(`https://api.github.com/orgs/nuxt`);
+// const { data: repoData } = await useFetch(`https://api.github.com/orgs/nuxt/repos`);
+
+/**
+ * 串接多支 API 建議使用的方法
+ */
+const [{ data: orgsData }, { data: reposData }] = await Promise.all([
+  useFetch('https://api.github.com/orgs/nuxt'),
+  useFetch('https://api.github.com/orgs/nuxt/repos'),
+]);
+
+</script>
+
+<style scoped>
+
+</style>
