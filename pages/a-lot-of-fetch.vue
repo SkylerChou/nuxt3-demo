b32bed0ddd59d8c1c0c5482b36671afd046d8592
Skyler_Chou <skyler_chou@donkeymove.com>
2024-02-15 14:45:58 +0800

    章節 3-6 - Server Side Rendering - useAsyncData + axios

--- pages/a-lot-of-fetch.vue
+++ pages/a-lot-of-fetch.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <h1>多個 API 請求</h1>
+    <!-- <h1>多個 API 請求</h1>
     <div>{{ orgsData }}</div>
     <div>------------------</div>
-    <div>{{ reposData }}</div>
+    <div>{{ reposData }}</div> -->
+    <h1>Axios</h1>
+    <div>{{ data }}</div>
   </div>
 </template>
 
 <script setup>
+import axios from 'axios';
 /**
  * API 少的時候使用此方式，還不會有什麼問題
  */
@@ -17,10 +20,21 @@
 /**
  * 串接多支 API 建議使用的方法
  */
-const [{ data: orgsData }, { data: reposData }] = await Promise.all([
-  useFetch('https://api.github.com/orgs/nuxt'),
-  useFetch('https://api.github.com/orgs/nuxt/repos')
-]);
+// const [{ data: orgsData }, { data: reposData }] = await Promise.all([
+//   useFetch('https://api.github.com/orgs/nuxt'),
+//   useFetch('https://api.github.com/orgs/nuxt/repos')
+// ]);
+
+/**
+ * 通常用在舊專案更新，沒有特需的需求建議使用內建的 fetch 就好
+ *
+ * ※ 注意
+ * 使用 axios 的話，最後要回傳 response data 的資料，如果回傳整個 axios.get 會直接壞掉
+ */
+const { data } = await useAsyncData('axiostest', async () => {
+  const res = await axios.get('https://api.github.com/orgs/nuxt');
+  return res.data;
+});
 </script>
 
 <style scoped></style>
