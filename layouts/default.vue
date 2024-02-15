948179b8495eecb4b4549548bdbee7c4648a43f3
Skyler_Chou <skyler_chou@donkeymove.com>
2024-02-15 18:39:39 +0800

    章節 4-6 - 客製化換頁 Loading (客製化) - 2

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,22 @@
 <script setup>
 import Header from 'components/Utils/Header.vue';
 import Footer from 'components/Utils/Footer.vue';
+
+const nuxtApp = useNuxtApp();
+const isLoading = ref(false);
+nuxtApp.hook('page:start', () => {
+  isLoading.value = true;
+});
+nuxtApp.hook('page:finish', () => {
+  isLoading.value = false;
+});
 </script>
 
 <template>
   <div>
-    <NuxtLoadingIndicator color="#f00" :height="10" :throttle="0" />
+    <!-- <NuxtLoadingIndicator color="#f00" :height="10" :throttle="0" /> -->
+
+    <h1 v-show="isLoading">Loading</h1>
 
     <Header />
     <!-- slot 可以想像成 router-view 的功能，才能顯示頁面畫面 -->
