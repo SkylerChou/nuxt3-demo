56be4aca4feb0240f0f4e5109f7071ecdaa7adbd
Skyler_Chou <skyler_chou@donkeymove.com>
2024-02-17 16:04:44 +0800

    章節 5-3 - Pinia 的小技巧 - storeToRefs

--- components/VoteCard.vue
+++ components/VoteCard.vue
@@ -1,15 +1,23 @@
 <script setup>
+import { storeToRefs } from 'pinia';
 import VoteBtn from 'components/VoteBtn.vue';
+/**
+ * ※ Pinia 的小技巧
+ * ref、computed 需要 storeToRefs
+ * reactive 、function 不需要，可以直接解構取出
+ */
 const store = useVoteStore();
+const { voteData, isFetch } = storeToRefs(store);
+const { addVote } = store;
 </script>
 <template>
-  <div v-for="vote in store.voteData" :key="vote.name" class="card">
+  <div v-for="vote in voteData" :key="vote.name" class="card">
     <div class="card_info">
       <img :src="vote.path" :alt="vote.name" />
       <h1>{{ vote.name }}</h1>
     </div>
-    <VoteBtn @click="store.addVote(vote.name)">
-      {{ store.isFetch ? '投票中' : vote.count0 }}
+    <VoteBtn @click="addVote(vote.name)">
+      {{ isFetch ? '投票中' : vote.count }}
     </VoteBtn>
   </div>
 </template>
