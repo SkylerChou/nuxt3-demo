9d0b4b5f7ef96a0a093b74d0ac9387c1dbb52678
Skyler_Chou <skyler_chou@donkeymove.com>
2024-02-17 11:25:47 +0800

    章節 4-7 - 404 頁面處理

---
+++ pages/[...slug].vue
@@ -0,0 +1,16 @@
+<!-- 
+  固定名稱，所有找不到頁面的網址都會進入的這頁
+  不要再 page/ 底下建立動態路由，不然會搶走[...slug].vue 的優先權！
+-->
+<script setup>
+const route = useRoute();
+const event = useRequestEvent();
+setResponseStatus(event, 404);
+</script>
+
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+    <p>{{ route.params.slug }}</p>
+  </div>
+</template>
